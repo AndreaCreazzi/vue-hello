e26c0498eb8a93e1a8197feae73fa73307d98931
Andrea Creazzi <andreacreazzi@libero.it>
2023-06-16 14:00:03 +0200

    add some comments

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+// controllo vue
 const { createApp } = Vue
 
-createApp({
+// vue
+const app = createApp({
     data() {
       return {
         greeting : ``,
         pic : `https://th.bing.com/th/id/OIP.N8JsMyHQFiHkOq-ppMCokAHaCO?w=336&h=105&c=7&r=0&o=5&dpr=1.5&pid=1.7`
       }
     }
-  }).mount('#root')
+  })
+  
+// stampa vue
+  app.mount('#root')
