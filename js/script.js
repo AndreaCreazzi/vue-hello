53f940d42b22052abda15801b19c3ce7ae1ca7b2
Andrea Creazzi <andreacreazzi@libero.it>
2023-06-16 13:56:56 +0200

    add vue (perfect) - h1 - pic (benvenuto)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        
+        greeting : ``,
+        pic : `https://th.bing.com/th/id/OIP.N8JsMyHQFiHkOq-ppMCokAHaCO?w=336&h=105&c=7&r=0&o=5&dpr=1.5&pid=1.7`
       }
     }
   }).mount('#root')
